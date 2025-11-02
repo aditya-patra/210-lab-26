bdc7f74272e41adcd0b1a40bcb6bab7527b3e4d1
Aditya Patra <adityabpatra@gmail.com>
2025-11-02 09:58:27 -0800

    continuing to create other race functions

--- main.cpp
+++ main.cpp
@@ -9,10 +9,10 @@
 using namespace std; 
 using namespace std::chrono;
 
-tuple<int, int, int, int> runReading();
-tuple<int, int, int, int> runSorting();
-tuple<int, int, int, int> runInserting();
-tuple<int, int, int, int> runDeleting();
+tuple<int, int, int> runReading();
+tuple<int, int, int> runSorting();
+tuple<int, int, int> runInserting();
+tuple<int, int, int> runDeleting();
 
 int main() {
     // first row of each array is for storing the latest race results
@@ -21,7 +21,7 @@ int main() {
     int sorting[2][3];
     int inserting[2][3];
     int deleting[2][3];
-    auto [temp1, temp2, temp3, temp4] = runRace();
+    auto [temp1, temp2, temp3] = runReading();
 
     //output
     cout << "Operation    Vector    List    Set" << endl;
@@ -32,7 +32,59 @@ int main() {
     return 0;
 }
 
-tuple<int, int, int, int> runRace() {
+//function runs race to see how long each type takes to read a set of codes
+tuple<int, int, int> runReading() {
+    
+    string code_lst[20000];
+    string curr_code;
+    ifstream file("codes.txt");
+
+    int reading[3];
+    int sorting[3];
+    int inserting[3];
+    int deleting[3];
+    set<string> set1;
+    vector<string> vec1;
+    list<string> lst1;
+
+    for(int i = 0; getline(file, curr_code); i++) {
+        code_lst[i] = curr_code;
+    }
+
+    // reading  test
+    // set reading
+    auto start = high_resolution_clock::now();
+    for(int i = 0; i < 20000; i++) {
+        set1.insert(code_lst[i]);
+    }
+    auto end = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(end - start);
+    reading[0] = duration.count();
+
+    // vector reading
+    start = high_resolution_clock::now();
+    for(int i = 0; i < 20000; i++) {
+        vec1.push_back(code_lst[i]);
+    }
+    end = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(end - start);
+    reading[1] = duration.count();
+
+    // list reading
+    start = high_resolution_clock::now();
+    for(int i = 0; i < 20000; i++) {
+        lst1.push_back(code_lst[i]);
+    }
+    end = high_resolution_clock::now();
+    duration = duration_cast<microseconds>(end - start);
+    reading[2] = duration.count();
+    tuple<int, int, int> tup = {reading[0], reading[1], reading[2]};
+    return tup;
+}
+
+
+//function runs race to see how long each type takes to read a set of codes
+tuple<int, int, int> runRace() {
     
     string code_lst[20000];
     string curr_code;
